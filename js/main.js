84cd3fb038c4742081a1d31668d457aa0e40f167
aoikubo <sakubo8440@gmail.com>
2021-02-12 03:22:53 +0900

    first travel

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+$(function () {
+  $('#openModal').click(function(){
+      $('#modalArea').fadeIn();
+  });
+  $('#closeModal , #modalBg').click(function(){
+    $('#modalArea').fadeOut();
+  });
+});
