c68b1b9208efcdf63d1152c35e19de11ec6f9648
aoikubo <sakubo8440@gmail.com>
2021-02-12 17:00:27 +0900

    add content

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ $(function () {
   $('#closeModal , #modalBg').click(function(){
     $('#modalArea').fadeOut();
   });
+
+  let $hoge = $('#js-rain');
+	$hoge.raindrops({
+		color: '#fff',
+		waveHeight: 100,
+		canvasHeight: 40,
+		rippleSpeed: 0.01,
+		rippleSpeed: 0.05,
+		density: 0.04,
+	});
 });
